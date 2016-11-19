4730703b91bab5a3867f4a15ddff93fa5b334e39
rastopyr <rastopyr@gmail.com>
2016-11-20 00:13:09 +0200

    Revert to normal baseurl in dev env

--- app/mirage/config.js
+++ app/mirage/config.js
@@ -1,5 +1,4 @@
 export default function() {
-  // this.namespace = '';
   this.timing = 400;
 
   /*
--- app/router.js
+++ app/router.js
@@ -1,12 +1,14 @@
 import Ember from 'ember';
 import config from './config/environment';
 
+console.log(config.locationType);
+
 const Router = Ember.Router.extend({
   location: config.locationType
 });
 
 Router.map(function() {
-  this.route('index');
+  this.route('index', { path: '/' });
   this.route('contacts', function() {
     this.route('contact', { path: ':id' });
   });
